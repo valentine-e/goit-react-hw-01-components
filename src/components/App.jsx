2efbe6a41790cfbd5ebd00b3f00a91a421165d8f
valentine-e <valia101112@gmail.com>
2023-10-08 14:23:26 +0300

    markup-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import Profile from './profile/profile.js';
+import user from './profile/profile.json';
+
+import Statistics from './stats/statistics.js';
+import statsData from './stats/statistics.json';
+
+import FriendsList from './friendsList/friendsList.js';
+import friendsData from './friendsList/friendsList.json';
+
+import TransactionHistory from './transactions/transactionHistory.js';
+import transactionsData from './transactions/transactionHistory.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework haha
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={statsData} />
+      <FriendsList friends={friendsData} />
+      <TransactionHistory items={transactionsData} />
     </div>
   );
 };
